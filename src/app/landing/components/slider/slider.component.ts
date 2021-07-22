d46f25593015b052f16f2f8a416a428d3dae465f
Svetlana <sss041296@mail.ru>
2021-07-22 21:59:04 +0400

    F-01 slider

--- src/app/landing/components/slider/slider.component.ts
+++ src/app/landing/components/slider/slider.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
+  currentSlide = 1;
+  private countSlides = 4;
 
+  prev() {
+    if (this.currentSlide === 1) {
+      this.currentSlide = this.countSlides;
+    } else {
+      this.currentSlide = this.currentSlide - 1;
+    }
+  }
+
+  next() {
+    if (this.currentSlide === this.countSlides) {
+      this.currentSlide = 1;
+    } else {
+      this.currentSlide = this.currentSlide + 1;
+    }
+  }
+
+  getTransform(): string {
+    return `translateX(-${100 / this.countSlides * (this.currentSlide - 1)}%)`;
+  }
+
+  openSlide(num: number) {
+    this.currentSlide = num;
+  }
 }
